1e593dc48346be2d3649b3f279fd37515b732091
Chetan Takyar <takyarchetan@gmail.com>
2020-09-12 10:52:13 +1200

    Added proxy and fetching weather info

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-const input = ()=>{
-    document.getElementById("cityInputBox").value;
+function fetchWeather(input) {
+  const proxyURI = "https://cors-anywhere.herokuapp.com/";
+  const URI = proxyURI+"https://www.metaweather.com/api/location/search/?query=" + input;
+	const fetchPromise = fetch(URI, {
+		headers: {
+			Accept: "application/json",
+		},
+	});
+	const StreamPromise = fetchPromise.then((weatherData) => weatherData.json());
+	StreamPromise.then((weatherJSON) => {
+		const woeid = weatherJSON[0].woeid;
+    
+		const weatherinfoURI = `https:www.metaweather.com/api/location/${woeid}/`;
+		const allInfo = fetch(weatherinfoURI, {
+			headers: {
+				Accept: "application/json",
+			},
+    });
+    const allInfoStream = allInfo.then(data => data.json());
+
+    allInfoStream.then((allData) => {
+      console.log(allData.title);
+
+    })
+	});
 }
 
-function fetchWeather(input) {
-  const URI = "/api/location/search/?query=" + input;
-  const fetchPromise = fetch(URI, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
-
-
+function cityinput() {
+	cityName = document.getElementById("cityInputBox").value;
+	return fetchWeather(cityName);
 }
